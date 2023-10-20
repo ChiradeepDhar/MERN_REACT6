f92db4ebfe2eff1e3c774983cc7bef9baa08daab
Chiradeep Dhar <115375978+ChiradeepDhar@users.noreply.github.com>
2023-10-20 16:49:28 +0530

    Add files via upload

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Contact = () => (
+  <section>
+    <h2>Contact Me</h2>
+    <p>Email: dhar.chiradeep@gmail.com</p>
+    <p>LinkedIn:https://www.linkedin.com/in/chiradeep-dhar-a1986b267/</p>
+  </section>
+);
+
+export default Contact;
